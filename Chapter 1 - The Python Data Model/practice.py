4a3c6590cfdde6218d7828443cebe9871efbf474
Chris <chriscottone1@gmail.com>
2026-03-31 16:34:38 -0400

    added a class to implement math dunder methods

--- Chapter 1 - The Python Data Model/practice.py
+++ Chapter 1 - The Python Data Model/practice.py
@@ -48,6 +48,38 @@ class Playlist:
                 string += f"\n{str}"
         return string
 
+
+# simple Number class for testing mathematical dunders
+class Number:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return f"Number({self.value})"
+
+    def __abs__(self):
+        return abs(self.value)
+
+    def __add__(self, other):
+        return Number(self.value + other)
+
+    def __sub__(self, other):
+        return Number(self.value - other)
+
+    def __mul__(self, other):
+        return Number(self.value * other)
+
+    def __truediv__(self, other):
+        return Number(self.value / other)
+
+    def __bool__(self):
+        return bool(self.value)
+
+
+# example code
 playlist = Playlist()
 print(playlist)
 print(f"Our playlist is {len(playlist)} songs long.")
@@ -59,6 +91,17 @@ for index, song in enumerate(playlist):
             print(key)
 
 print(f"The last song in our playlist is....{playlist[-1]}")
+
+num1 = Number(1)
+num0 = Number(0)
+num2 = Number(2)
+
+print(num1 + 10)
+print(num1 - 10)
+print(num1 * 10)
+print(num1 / 10)
+print(bool(num1))
+print(bool(num0))
 
 
 # sequence ops like random.choice(), or index-based element selection don't work on dictionaries.
